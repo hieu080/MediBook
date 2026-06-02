c55c163e5305f577f6ff2764f2c5367b17d05884
hieu-pt <hieu.pt@tinasoft.com.vn>
2026-06-02 22:28:08 +0700

    feature: base cho identity service để dựng JWT v2.2

--- identity-service/src/main/java/com/identityservice/IdentityServiceApplication.java
+++ identity-service/src/main/java/com/identityservice/IdentityServiceApplication.java
@@ -3,7 +3,7 @@ package com.identityservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.identityservice", "com.sharekernel"})
 public class IdentityServiceApplication {
 
     public static void main(String[] args) {
