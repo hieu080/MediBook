5b92c122b8cbf2fc2ad5d5c8bd6c7f95b3527cee
hieu-pt <hieu.pt@tinasoft.com.vn>
2026-06-26 09:49:09 +0700

    feature: [Keycloak] Refactor Gateway

--- api-gateway/src/main/java/com/apigateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/apigateway/security/SecurityConfig.java
@@ -20,7 +20,6 @@ public class SecurityConfig {
                                 "/api/v1/auth/login",
                                 "/api/v1/auth/refresh",
                                 "/api/v1/auth/logout",
-                                "/api/v1/auth/.well-known/jwks.json",
                                 "/actuator/health",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
